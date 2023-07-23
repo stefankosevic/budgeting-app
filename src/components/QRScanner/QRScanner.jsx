5f5fe795470ad07e68413ba4eba1812b5c8329cc
stefankosevic <skosevic@gmail.com>
2023-07-23 18:34:13 +0200

    BA | QR

--- src/components/QRScanner/QRScanner.jsx
+++ src/components/QRScanner/QRScanner.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import axios from "axios";
+import { TABS } from "../../lib/constants/tabs";
 
 const QRScanner = () => {
   const [data, setData] = useState("No result");
@@ -9,19 +10,32 @@ const QRScanner = () => {
   useEffect(() => {
     if (!data.includes("http")) return;
 
-    try {
-      const { data: total } = axios.get(
-        process.env.REACT_APP_API_URL + "/user/api/scan",
-        {
-          params: {
-            url: data,
-          },
-        }
-      );
-      setTotalCash(total);
-    } catch (err) {
-      setTotalCash(err);
-    }
+    const getQRAmount = async () => {
+      try {
+        const { data: total } = await axios.get(
+          process.env.REACT_APP_API_URL + "/user/api/scan",
+          {
+            params: {
+              url: data,
+            },
+          }
+        );
+        setTotalCash(total);
+        const totalRacun = data.data;
+        await axios.post(process.env.REACT_APP_API_URL + "/user/api/balance", {
+          title: "Racun",
+          date: new Date().toLocaleDateString(),
+          timestamp: new Date(),
+          amount: totalRacun,
+          userId: localStorage.getItem("userId"),
+          type: TABS.ONE_TIME_EXPENSE,
+        });
+      } catch (err) {
+        setTotalCash(err);
+      }
+    };
+
+    getQRAmount();
   }, [data]);
 
   return (
